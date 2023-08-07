9132aeeafbff683aeab537bfba66415d7e4cc2e7
Travis Saucier <travis.saucier@sourcefuse.com>
2023-08-07 11:53:15 -0400

    remove unneeded lock file. add updates provider reqs.

--- example/main.tf
+++ example/main.tf
@@ -2,12 +2,12 @@
 ## defaults
 ################################################################################
 terraform {
-  required_version = "~> 1.3, < 2.0.0"
+  required_version = ">= 1.3, < 2.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 4.0"
     }
   }
 }
